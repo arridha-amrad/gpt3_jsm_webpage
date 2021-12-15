99f4519acaffb9abe163d90c4e5b55bfb4bc30c4
Arridha Amrad <ari@air.local>
2021-12-15 15:31:35 +0700

    left footer

--- src/components/cta/CTA.tsx
+++ src/components/cta/CTA.tsx
@@ -6,7 +6,7 @@ const CTA = () => {
       <div className="cta">
         <div className="cta__left">
           <p>Request Early Access to Get Started</p>
-          <h1>Register today & start exploring the endless possiblities.</h1>
+          <h1>Register today & start exploring the endless possibilities.</h1>
         </div>
         <div className="cta__right">
           <button className="btn">Get Started</button>
